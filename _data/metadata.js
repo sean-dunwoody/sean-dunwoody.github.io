f50f08020d740784777200f6ae5ab5963c98298a
Sean Dunwoody <sean_dunwoody@yahoo.com>
2024-12-30 18:44:48 +0000

    Setup blog site using the eleventy base blog template

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Sean Dunwoody",
+	url: "https://sean.dunwoody.me/",
+	language: "en-GB",
+	description: "Personal blog site for Sean Dunwoody.",
+	author: {
+		name: "Sean Dunwoody",
+		email: "sean@dunwoody.me",
+		url: "https://sean.dunwoody.me/about-me/"
+	}
+}
